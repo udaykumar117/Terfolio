b4a50df2242ea52b84c2f7049e850c7fc6377f89
udaykumar117 <72568252+udaykumar117@users.noreply.github.com>
2021-09-28 09:30:47 -0400

    JS

---
+++ js/data.js
@@ -0,0 +1,27 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Uday Kumar Vurukonda'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Uday Kumar Vurukonda'},
+    {name: 'Email', value: 'udaykumar6273gmail.com'},
+    {name: 'Phone', value: '+1 201-993-5681'},
+    {name: 'Country', value: 'United States of America'},
+    {name: 'City', value: 'Boston'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'udaykumar117'}
+  ],
+  projects: [
+    {name: 'Online Book Store', value: 'https://github.com/udaykumar117/Online-Book-Store'},
+    {name: 'Portfolio', value: 'https://udaykumar117.github.io/'},
+    {name: 'Quiz System', value: 'https://github.com/udaykumar117/Quiz-System'},
+    {name: 'Piano Keyboard With Preset Songs', value: 'https://github.com/udaykumar117/Piano-Keyboard-With-Preset-Songs'}
+  ],
+  resume: 'UdayKumar.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://www.linkedin.com/in/uday-kumar7/'},
+    {name: 'GitHub', value: 'https://github.com/udaykumar117'},
+    {name: 'Website', value: 'https://udaykumar117.github.io/'}
+  ]
+}
